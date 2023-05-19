3b3d0328264c57a80e04cfd8401f0a9ed8248bc5
mohammad-a-tab <mohammad.programmer.tab@gmail.com>
2023-05-20 00:05:13 +0330

    create ikco file

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { IkcoController } from './ikco/ikco.controller';
+import { IkcoService } from './ikco/ikco.service';
+import { IkcoModule } from './ikco/ikco.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [IkcoModule],
+  controllers: [AppController, IkcoController],
+  providers: [AppService, IkcoService],
 })
 export class AppModule {}
---
+++ src/ikco/ikco.controller.ts
@@ -0,0 +1,4 @@
+import { Controller } from '@nestjs/common';
+
+@Controller('ikco')
+export class IkcoController {}
