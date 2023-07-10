1938ae2ea6c118264ca5b82c15a761d4e3fcb74a
mohammad-a-tab <mohammad.programmer.tab@gmail.com>
2023-07-10 23:51:59 +0330

    fix ikco entity by type cantent interface

--- src/ikco/ikco.entity.ts
+++ src/ikco/ikco.entity.ts
@@ -1,18 +1,19 @@
+import { Content } from "src/interface/content.interface";
 import { Column, CreateDateColumn, Entity, PrimaryGeneratedColumn, ObjectId, UpdateDateColumn } from "typeorm";
 @Entity('users')
 export class User {
     @PrimaryGeneratedColumn('uuid')
     id: ObjectId;
-    @Column({ nullable: true })
+    @Column()
     car_name: string;
-    @Column({ nullable: false, unique: true })
-    mechanical: string;
-    @Column({ nullable: false })
-    Injector: string;
-    @Column({ nullable: false })
-    Wiring: string;
-    @Column({ default: "" })
-    Engine: string;
+    @Column()
+    mechanical: Content;
+    @Column()
+    Injector: Content;
+    @Column()
+    Wiring: Content;
+    @Column()
+    Engine: Content;
     @CreateDateColumn({ name: 'created_at', type: 'timestamp' })
     createdAt: Date;
     @UpdateDateColumn({ name: 'updated_at', type: 'timestamp' })
