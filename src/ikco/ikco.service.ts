e55f5b37663afcf55d5c868752a4364b991d2357
mohammad-a-tab <mohammad.programmer.tab@gmail.com>
2023-07-11 00:17:44 +0330

    make changes

--- src/ikco/ikco.service.ts
+++ src/ikco/ikco.service.ts
@@ -1,4 +1,9 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { Ikco } from './ikco.model';
 
 @Injectable()
-export class IkcoService {}
+export class IkcoService {
+    constructor(@InjectModel(Ikco.name) private ikcoModel: Model<Ikco>) {}
+}
