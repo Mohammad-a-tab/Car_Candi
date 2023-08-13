f72f80f98e34373f67d8a12cc8bb08bb3b703393
mohammad-a-tab <mohammad.programmer.tab@gmail.com>
2023-08-13 23:13:17 +0330

    create user folder

--- src/app.module.ts
+++ src/app.module.ts
@@ -13,6 +13,7 @@ import { ToyotaModule } from './toyota/toyota.module';
 import { ZamyadModule } from './zamyad/zamyad.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
@@ -28,7 +29,7 @@ import { AuthModule } from './auth/auth.module';
     ParsCarModule, 
     RamackCarModule, 
     ToyotaModule, 
-    ZamyadModule, AuthModule
+    ZamyadModule, AuthModule, UsersModule
   ],
 })
 export class AppModule {}
